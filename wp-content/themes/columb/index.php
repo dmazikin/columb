a75f8b52545ba404366695bae733bbb67b7f473f
Dmitry <mazikindima@mail.ru>
2023-02-22 21:17:14 +0300

    feat: Добавил функцию динамического слайдера

--- wp-content/themes/columb/index.php
+++ wp-content/themes/columb/index.php
@@ -20,21 +20,37 @@ get_header();
 </div>
 <div class="swiper mySwiper">
   <div class="swiper-wrapper">
-    <div class="swiper-slide">
-      <img src="<?php echo get_template_directory_uri() .  '/img/1.png' ?>" />
-      <div class="slider-text">Открываем Сочи вместе с вами!</div>
-    </div>
-    <div class="swiper-slide">
-      <img src="<?php echo get_template_directory_uri() .  '/img/2.png' ?>" />
-      <div class="slider-text">Открываем Сочи вместе с вами!</div>
-    </div>
-    <div class="swiper-slide">
-      <img src="<?php echo get_template_directory_uri() .  '/img/3.png' ?>" />
-      <div class="slider-text">Открываем Сочи вместе с вами!</div>
-    </div>
+    <?php
+    // параметры по умолчанию
+    $my_posts = get_posts(array(
+      'numberposts' => -1,
+      'category_name'    => 'slider_head',
+      'orderby'     => 'date',
+      'order'       => 'ASC',
+      'post_type'   => 'post',
+      'suppress_filters' => true, // подавление работы фильтров изменения SQL запроса
+    ));
+
+    global $post;
+
+    foreach ($my_posts as $post) {
+      setup_postdata($post);
+    ?>
+      <div class="swiper-slide">
+        <img src="<?php the_field('swiper_head_img') ?>" />
+        <div class="slider-text">Открываем Сочи вместе с вами!</div>
+      </div>
+    <?php
+      // формат вывода the_title() ...
+    }
+
+    wp_reset_postdata(); // сброс
+    ?>
   </div>
   <div class="swiper-button-next"><img src="<?php echo get_template_directory_uri() .  '/img/right_button_slide.png' ?>" alt="arrow right" /></div>
   <div class="swiper-button-prev"><img src="<?php echo get_template_directory_uri() . '/img/left_button_slide.png' ?>" alt="arrow left" /></div>
 </div>
+
 <?php
 get_footer();
+?>
