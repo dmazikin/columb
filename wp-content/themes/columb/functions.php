8af21e8683a7b288fc553479c872e781391f1a93
Dmitry <mazikindima@mail.ru>
2023-02-24 12:45:13 +0300

    feat: Отключение стилей вукомерц

--- wp-content/themes/columb/functions.php
+++ wp-content/themes/columb/functions.php
@@ -212,3 +212,5 @@ add_action('after_setup_theme', 'columbtheme_add_woocommerce_support');
 add_theme_support('wc-product-gallery-zoom');
 add_theme_support('wc-product-gallery-lightbox');
 add_theme_support('wc-product-gallery-slider');
+//Отключение стилей вукомерц
+add_filter('woocommerce_enqueue_styles', '__return_false');
