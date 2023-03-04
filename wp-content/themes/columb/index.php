bb1fbc553420f3407c5586e24d6800e10b9a1bf7
Dmitry <mazikindima@mail.ru>
2023-03-04 18:47:53 +0300

    feat: Добавил экскурсии,трансфер,вип и mice

--- wp-content/themes/columb/index.php
+++ wp-content/themes/columb/index.php
@@ -59,7 +59,167 @@ get_header();
     <img class="right-image" src="<?= the_field('index_about_img') ?>" />
   </div>
 </div>
-<?php woocommerce_content(); ?>
+<div class="cards container">
+  <div class="more-button-row">
+    <div class="left-text head-text">
+      <span><?php echo the_field('section_product_title') ?></span>
+      <div class="line"></div>
+    </div>
+    <button class="right-button more-button">Показать больше</button>
+  </div>
+  <div class="card-container">
+    <?php
+    $loop = new WP_Query(array(
+      'product_cat' => 'excursion',
+      'post_type' => 'product',
+      'posts_per_page' => 12,
+      'orderby' => 'menu_order',
+      'order' => 'ASC',
+    ));
+
+    while ($loop->have_posts()) : $loop->the_post(); ?>
+
+      <div class="card">
+        <div class="card-top">
+          <?php echo the_post_thumbnail(); ?>
+        </div>
+        <h2 class="card-title">
+          <a href="<?php the_permalink(); ?>">
+            <?php the_title(); ?>
+          </a>
+        </h2>
+        <?php the_content(); ?>
+        <p class="price">
+          <?php woocommerce_template_loop_price(); ?>
+        </p>
+        <?php woocommerce_template_loop_add_to_cart(); ?>
+      </div>
+
+    <?php endwhile;
+    wp_reset_postdata(); ?>
+  </div>
+</div>
+<div class="transfer container">
+  <div class="head-text">
+    <span><?php echo the_field('title_transfer') ?></span>
+    <div class="line"></div>
+  </div>
+  <?php echo the_field('section_transfer_desc') ?>
+  <div class="transfer-container">
+    <div class="subhead-text"><?php echo the_field('individual-transfer_subtitle', 5) ?></div>
+    <div class="transfer-wrapper">
+      <?php if (have_rows('individual-transfer')) : ?>
+        <?php while (have_rows('individual-transfer')) : the_row();
+
+          // переменные
+          $individualImg = get_sub_field('individual-transfer_img');
+          $individualTitle = get_sub_field('individual-transfer_title');
+          $individualDesc = get_sub_field('individual-transfer_desc');
+        ?>
+          <div class="transfer-card">
+            <img src="<?php echo $individualImg; ?>" alt="image">
+            <p class="car-name"><?php echo $individualTitle; ?></p>
+            <p class="car-text"><?php echo $individualDesc ?></p>
+            <a href="<?php echo get_site_url() . '/transfer'; ?>" class="card-button">Забронировать</a>
+
+          </div>
+        <?php endwhile; ?>
+      <?php endif; ?>
+    </div>
+  </div>
+  <div class="transfer-container">
+    <div class="subhead-text"><?php echo the_field('group-transfer_subtitle', 5) ?></div>
+    <div class="transfer-wrapper">
+      <?php if (have_rows('group-transfer')) : ?>
+        <?php while (have_rows('group-transfer')) : the_row();
+
+          // переменные
+          $individualImg = get_sub_field('group-transfer_img');
+          $individualTitle = get_sub_field('group-transfer_title');
+          $individualDesc = get_sub_field('group-transfer_desc');
+        ?>
+          <div class="transfer-card">
+            <img src="<?php echo $individualImg; ?>" alt="image">
+            <p class="car-name"><?php echo $individualTitle; ?></p>
+            <p class="car-text"><?php echo $individualDesc ?></p>
+            <a href="<?php echo get_site_url() . '/transfer'; ?>" class="card-button">Забронировать</a>
+
+          </div>
+        <?php endwhile; ?>
+      <?php endif; ?>
+    </div>
+  </div>
+</div>
+<div class="booking container">
+  <div class="head-text">
+    <span><?php echo the_field('section_reserv_title'); ?></span>
+    <div class="line"></div>
+  </div>
+  <?php if (have_rows('reserv_slider')) : ?>
+
+    <div class="swiper bookingSwiper">
+      <div class="swiper-wrapper">
+        <?php while (have_rows('reserv_slider')) : the_row();
+
+          // переменные
+          $reservImg = get_sub_field('reserv_slider_img', 5);
+          $reservTitle = get_sub_field('reserv_slider_title', 5);
+
+        ?>
+          <div class="swiper-slide">
+            <div class="booking-card">
+              <img src="<?php echo  $reservImg['url']; ?>" alt="<?php echo $reservImg['alt']; ?>" />
+              <p><?php echo $reservTitle; ?></p>
+            </div>
+          </div>
+        <?php endwhile; ?>
+      </div>
+      <div class="swiper-button-next"><img src="<?php echo get_template_directory_uri() .  '/img/right_button_slide.png' ?>" alt="arrow right" /></div>
+      <div class="swiper-button-prev"><img src="<?php echo get_template_directory_uri() . '/img/left_button_slide.png' ?>" alt="arrow left" /></div>
+    </div>
+  <?php endif; ?>
+</div>
+<div class="service container">
+  <div class="head-text">
+    <span><?php echo the_field('section_mice_title', 5) ?></span>
+    <div class="line"></div>
+  </div>
+  <div class="service-wrapper">
+    <div class="service-text text">
+      <?php echo the_field('mice_desc', 5) ?>
+    </div>
+    <div class="service-gallery">
+      <?php
+      $miceImages = get_field('mice_gallery', 5);
+      if ($miceImages) :
+      ?>
+        <?php foreach ($miceImages as $miceImage) : ?>
+          <img src="<?php echo $miceImage['url']; ?>" alt="img" />
+        <?php endforeach; ?>
+      <?php endif; ?>
+    </div>
+  </div>
+
+  <div class="head-text">
+    <span><?php echo the_field('section_vip_title', 5) ?></span>
+    <div class="line"></div>
+  </div>
+  <div class="service-wrapper">
+    <div class="service-text text">
+      <?php echo the_field('vip_desc', 5) ?>
+    </div>
+    <div class="service-gallery">
+      <?php
+      $vipImages = get_field('vip_gallery', 5);
+      if ($vipImages) :
+      ?>
+        <?php foreach ($vipImages as $vipImage) : ?>
+          <img src="<?php echo $vipImage['url']; ?>" alt="img" />
+        <?php endforeach; ?>
+      <?php endif; ?>
+    </div>
+  </div>
+</div>
 <?php
 get_footer();
 ?>
