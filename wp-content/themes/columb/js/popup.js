85f13c41ec916afff60ec42c843984808b399f79
Dmitry <mazikindima@mail.ru>
2023-03-05 18:48:12 +0300

    fix

--- wp-content/themes/columb/js/popup.js
+++ wp-content/themes/columb/js/popup.js
@@ -1,31 +1,21 @@
-const openPopup = document.getElementsByClassName('popup-open')[0];
-const closePopupByButton = document.getElementsByClassName('popup-close')[0];
-const closePopupByScreen = document.getElementsByClassName('popup-close-screen')[0];
-const popup = document.getElementsByClassName('popup-screen')[0];
-
+const openPopup = document.getElementsByClassName("popup-open")[0];
+const closePopupByButton = document.getElementsByClassName("popup-close")[0];
+const closePopupByScreen = document.getElementsByClassName("popup-close-screen")[0];
+const popup = document.getElementsByClassName("popup-screen")[0];
 
 // "order a call" menu
-openPopup.addEventListener('click', function (e) {
+openPopup.addEventListener("click", function (e) {
   e.preventDefault();
-  popup.classList.add('active');
-})
-
-closePopupByButton.addEventListener('click', () => {
-  popup.classList.remove('active');
-})
-
-// чтобы не реагировал на popup-inner
-document.querySelector('.popup-close-screen').addEventListener('click', function (event) {
-  if (event.target.classList.contains('popup-close-screen')) {
-    popup.classList.remove('active');
-  }
+  popup.classList.add("active");
 });
 
+closePopupByButton.addEventListener("click", function () {
+  popup.classList.remove("active");
+});
 
-
-
-
-
-
-
-
+// чтобы не реагировал на popup-inner
+document.querySelector(".popup-close-screen").addEventListener("click", function (event) {
+  if (event.target.classList.contains("popup-close-screen")) {
+    popup.classList.remove("active");
+  }
+});
