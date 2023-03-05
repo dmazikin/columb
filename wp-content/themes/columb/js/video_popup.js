69e5627311cd2c9c9d5e954a92777a8b777bd80b
Dmitry <mazikindima@mail.ru>
2023-03-05 18:48:54 +0300

    feat: Динамическая загрузка видео

--- wp-content/themes/columb/js/video_popup.js
+++ wp-content/themes/columb/js/video_popup.js
@@ -1,26 +1,15 @@
 // popup video на главной странице
-document.querySelectorAll('.video-open').forEach(function(button) {
-  button.addEventListener('click', function() {
-    document.querySelector('.video-popup').classList.add('active');
+document.querySelectorAll(".video-open").forEach(function (button) {
+  button.addEventListener("click", function () {
+    console.log(button.dataset.src);
+    document.querySelector(".video-popup").classList.add("active");
+    document.getElementById("youtube-frame").src = button.dataset.src;
   });
 });
 
-document.querySelector('.video-popup').addEventListener('click', function(e) {
-  if (!e.target.closest('.inner')) {
-    this.classList.remove('active');
+document.querySelector(".video-popup").addEventListener("click", function (e) {
+  if (!e.target.closest(".inner")) {
+    this.classList.remove("active");
+    document.getElementById("youtube-frame").src = "";
   }
 });
-//   Ниже код для того, что бы видео останавливалось при закрытии окна.
-
-const videoPopup = document.querySelector('.video-popup');
-const videoIframe = document.querySelector('iframe');
-const videoSrc = videoIframe.src; // сохроняем пред src
-
-videoPopup.addEventListener('click', (e) => {
-  if (!videoPopup.classList.contains('active')) {
-    videoIframe.src = videoSrc;
-  } else {
-    videoIframe.src = '';
-  }
-});
-
