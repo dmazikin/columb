079df010aa6446a26e50776219e9208dce272df1
Dmitry <mazikindima@mail.ru>
2023-03-07 11:03:43 +0300

    feat: Вывод корзины

--- wp-content/themes/columb/page-card.php
+++ wp-content/themes/columb/page-card.php
@@ -5,7 +5,11 @@
  */
 get_header();
 ?>
-
+<?php if (have_posts()) : ?>
+  <?php the_post(); ?>
+  <h2><?php the_title(); ?></h2>
+  <?php the_content(); ?>
+<?php endif; ?>
 <?php
 get_footer();
 ?>
