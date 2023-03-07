e46ab43c6750196bd37dc2ed005e1065de3025d1
Dmitry <mazikindima@mail.ru>
2023-03-07 19:05:21 +0300

    update

--- wp-content/themes/columb/index.php
+++ wp-content/themes/columb/index.php
@@ -164,11 +164,12 @@ get_header();
           $reservTitle = get_sub_field('reserv_slider_title', 5);
 
         ?>
-          <div class="booking-card">
-            <img src="<?php echo  $reservImg['url']; ?>" alt="<?php echo $reservImg['alt']; ?>" />
-            <p><?php echo $reservTitle; ?></p>
+          <div class="swiper-slide">
+            <div class="booking-card">
+              <img src="<?php echo  $reservImg['url']; ?>" alt="<?php echo $reservImg['alt']; ?>" />
+              <p><?php echo $reservTitle; ?></p>
+            </div>
           </div>
-
         <?php endwhile; ?>
       </div>
       <div class="swiper-button-next"><img src="<?php echo get_template_directory_uri() .  '/img/right_button_slide.png' ?>" alt="arrow right" /></div>
