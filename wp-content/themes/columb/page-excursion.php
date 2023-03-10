ff755aa3b7bd5863d396c107945852e328a54e9b
Dmitry <mazikindima@mail.ru>
2023-03-10 07:32:58 +0300

    feat: Хлебные крошки и вывод товаров

--- wp-content/themes/columb/page-excursion.php
+++ wp-content/themes/columb/page-excursion.php
@@ -5,7 +5,10 @@
  */
 get_header();
 ?>
-
+<div class="breadcrumbs-container container">
+  <?php woocommerce_breadcrumb(); ?>
+</div>
+<?php echo do_shortcode("[products category='excursion']"); ?>
 <?php
 get_footer();
 ?>
