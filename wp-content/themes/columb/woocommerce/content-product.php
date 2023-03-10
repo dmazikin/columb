31f2cf38d3682ba8223f30120ba41822ecb273dc
Dmitry <mazikindima@mail.ru>
2023-03-10 07:34:20 +0300

    feat: Изменил шаблон для карточек товара

--- wp-content/themes/columb/woocommerce/content-product.php
+++ wp-content/themes/columb/woocommerce/content-product.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying product content within loops
  *
@@ -15,53 +16,36 @@
  * @version 3.6.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 global $product;
 
 // Ensure visibility.
-if ( empty( $product ) || ! $product->is_visible() ) {
+if (empty($product) || !$product->is_visible()) {
 	return;
 }
+$product_published = $product->get_date_created(); //$product_published->date
 ?>
-<li <?php wc_product_class( '', $product ); ?>>
-	<?php
-	/**
-	 * Hook: woocommerce_before_shop_loop_item.
-	 *
-	 * @hooked woocommerce_template_loop_product_link_open - 10
-	 */
-	do_action( 'woocommerce_before_shop_loop_item' );
-
-	/**
-	 * Hook: woocommerce_before_shop_loop_item_title.
-	 *
-	 * @hooked woocommerce_show_product_loop_sale_flash - 10
-	 * @hooked woocommerce_template_loop_product_thumbnail - 10
-	 */
-	do_action( 'woocommerce_before_shop_loop_item_title' );
-
-	/**
-	 * Hook: woocommerce_shop_loop_item_title.
-	 *
-	 * @hooked woocommerce_template_loop_product_title - 10
-	 */
-	do_action( 'woocommerce_shop_loop_item_title' );
-
-	/**
-	 * Hook: woocommerce_after_shop_loop_item_title.
-	 *
-	 * @hooked woocommerce_template_loop_rating - 5
-	 * @hooked woocommerce_template_loop_price - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop_item_title' );
-
-	/**
-	 * Hook: woocommerce_after_shop_loop_item.
-	 *
-	 * @hooked woocommerce_template_loop_product_link_close - 5
-	 * @hooked woocommerce_template_loop_add_to_cart - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop_item' );
-	?>
-</li>
+<div <?php wc_product_class('card', $product); ?>>
+	<div class="card-top">
+		<?php echo $product->get_image('100%'); ?>
+		<?php if ($product->is_featured()) : ?>
+			<div class="marker yellow">
+				<img src="<?php echo get_template_directory_uri() . '/img/marker_yellow.png'; ?>" alt="" />
+				<p>Хит продаж</p>
+			</div>
+		<?php elseif (strtotime($product_published->date) < (time() - 86400 * 5)) : ?>
+			<div class="marker red">
+				<img src="<?php echo get_template_directory_uri() . '/img/marker_red.png' ?>" />
+				<p>Новинка</p>
+			</div>
+		<?php endif; ?>
+	</div>
+	<h2 class="card-title"><?php echo $product->get_title(); ?></h2>
+	<p class="card-text">
+		<?php echo $product->get_description(); ?>
+	</p>
+	<button class="card-button">
+		<a href="<?php echo $product->add_to_cart_url(); ?>"><?php echo $product->add_to_cart_text(); ?></a>
+	</button>
+</div>
