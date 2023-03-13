05db696c858319068fc99092390e9e0581c124fa
Dmitry <mazikindima@mail.ru>
2023-03-13 20:02:24 +0300

    feat: Фильтр экскурсий

--- wp-content/themes/columb/sidebar.php
+++ wp-content/themes/columb/sidebar.php
@@ -1,12 +1,24 @@
-<?php
-/**
- * The sidebar containing the main widget area
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package columb
- */
-
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
+<?php dynamic_sidebar('filter'); ?>
+<div class="container tours-chooser">
+  <div class="tour-btn active">
+    <p>Красная Поляна — Русская Швейцария<span> (плановые)</span></p>
+  </div>
+  <div class="tour-btn">
+    <p>Чарующая Абхазия<span> (плановые)</span></p>
+  </div>
+  <div class="tour-btn">
+    <p>Обзорные и панорамные экскурсии<span> (плановые)</span></p>
+  </div>
+  <div class="tour-btn">
+    <p>Природные экскурсии</p>
+  </div>
+  <div class="tour-btn">
+    <p>Тематические экскурсии</p>
+  </div>
+  <div class="tour-btn">
+    <p>Экскурсии для всей семьи</p>
+  </div>
+  <div class="tour-btn">
+    <p>Активный отдых</p>
+  </div>
+</div>
