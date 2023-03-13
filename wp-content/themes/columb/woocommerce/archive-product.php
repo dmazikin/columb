9a922013ec723cd7d3abe4b7e7e0a870432d057d
Dmitry <mazikindima@mail.ru>
2023-03-13 20:03:18 +0300

    feat: Изменил функционал страницы магазина

--- wp-content/themes/columb/woocommerce/archive-product.php
+++ wp-content/themes/columb/woocommerce/archive-product.php
@@ -18,7 +18,7 @@
 
 defined('ABSPATH') || exit;
 
-get_header('shop');
+get_header();
 
 /**
  * Hook: woocommerce_before_main_content.
@@ -27,21 +27,11 @@ get_header('shop');
  * @hooked woocommerce_breadcrumb - 20
  * @hooked WC_Structured_Data::generate_website_data() - 30
  */
-do_action('woocommerce_before_main_content');
-
 ?>
-
+	<?php woocommerce_breadcrumb(); ?>
+	<?php get_sidebar(); ?>
 <?php
 if (woocommerce_product_loop()) {
-
-	/**
-	 * Hook: woocommerce_before_shop_loop.
-	 *
-	 * @hooked woocommerce_output_all_notices - 10
-	 * @hooked woocommerce_result_count - 20
-	 * @hooked woocommerce_catalog_ordering - 30
-	 */
-	do_action('woocommerce_before_shop_loop');
 
 	woocommerce_product_loop_start();
 
@@ -49,44 +39,17 @@ if (woocommerce_product_loop()) {
 		while (have_posts()) {
 			the_post();
 
-			/**
-			 * Hook: woocommerce_shop_loop.
-			 */
-			do_action('woocommerce_shop_loop');
-
 			wc_get_template_part('content', 'product');
 		}
 	}
 
 	woocommerce_product_loop_end();
-
-	/**
-	 * Hook: woocommerce_after_shop_loop.
-	 *
-	 * @hooked woocommerce_pagination - 10
-	 */
-	do_action('woocommerce_after_shop_loop');
 } else {
-	/**
-	 * Hook: woocommerce_no_products_found.
-	 *
-	 * @hooked wc_no_products_found - 10
-	 */
-	do_action('woocommerce_no_products_found');
+
+	wc_no_products_found();
 }
+?>
 
-/**
- * Hook: woocommerce_after_main_content.
- *
- * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
- */
-do_action('woocommerce_after_main_content');
-
-/**
- * Hook: woocommerce_sidebar.
- *
- * @hooked woocommerce_get_sidebar - 10
- */
-do_action('woocommerce_sidebar');
-
-get_footer('shop');
+<?php
+get_footer();
+?>
