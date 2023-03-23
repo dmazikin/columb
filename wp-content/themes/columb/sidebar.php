6d80d2b2cea36a576db7be56833d5bd9dbd59a67
Dmitry <mazikindima@mail.ru>
2023-03-23 10:20:25 +0300

    refactor: Указал родителя экскурсий

--- wp-content/themes/columb/sidebar.php
+++ wp-content/themes/columb/sidebar.php
@@ -1,9 +1,9 @@
-<?php $product_categories = get_terms(array('taxonomy' => 'product_cat', 'hide_empty' => true)); ?>
+<?php $product_categories = get_terms(array('taxonomy' => 'product_cat', 'hide_empty' => false,'parent'=> 20,'order'=>'DESC')); ?>
 <?php if ($product_categories) : ?>
   <div class="container tours-chooser">
     <?php foreach ($product_categories as $category) : ?>
       <div class="tour-btn">
-        <p><a href="<?php echo get_term_link($category); ?>"><?php echo $category->name ?></a></p>
+        <a href="<?php echo get_term_link($category); ?>"><?php echo $category->name ?></a>
       </div>
     <?php endforeach; ?>
   </div>
