774ace2f07ebdfac9b75b418f935dc48894d2c2e
Dmitry <mazikindima@mail.ru>
2023-04-13 11:00:03 +0300

    feat: Для вывода документов

---
+++ wp-content/themes/columb/single.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package columb
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="container">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+      ?>
+
+			<h1 class='about-title'><?php the_title()?></h1>
+      <div class="content">
+      <?php the_content()?>
+      </div>
+<?php 
+		endwhile; // End of the loop.
+		?>
+	</div><!-- #primary -->
+
+<?php
+get_footer();
