9a492f663d921196f4cbd3e4a9f83c5cba4f7158
Dmitry <mazikindima@mail.ru>
2023-04-13 10:59:43 +0300

    refactor: Вывел только одни экскурсии для показа

--- wp-content/themes/columb/page-excursion.php
+++ wp-content/themes/columb/page-excursion.php
@@ -5,7 +5,31 @@
  */
 get_header();
 ?>
-<?php the_content(); ?>
+<?php woocommerce_breadcrumb(); ?>
+	<?php get_sidebar(); ?>
+<?php 
+  $paged = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
+  $Exc = new WP_Query(array(
+    'product_cat' => 'excursion',
+    'post_type' => 'product',
+    'posts_per_page' => -1,
+    'paged'=> $paged,
+    'order' => 'ASC',
+    'orderby' => 'name',
+    
+  ));
+  
+  $max_pages = $topExc->max_num_pages;
+
+woocommerce_product_loop_start();
+  while ($Exc->have_posts()) {
+    $Exc->the_post();
+    wc_get_template_part('content', 'product');
+  }
+woocommerce_product_loop_end();
+
+wp_reset_postdata();
+?>
 <?php
 get_footer();
 ?>
